9d42c1ee22977faba59d9056fba485dcb8617e55
Joao Felipe Rocha <joaofelipe.rocha@.yale.edu>
2025-03-25 12:41:18 -0400

    Minor changes

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ from src.graphs.builder import return_graph_data
 # my defaults are python main.py --data_dir data/ --output_dir wavelet_features/ --k_hop 1 
 # for hyperedge averaging: main.py --data_dir data/ --output_dir hyperedge_avg/ --k_hop 3 --hyperedge_features gene_expression --wavelets 0
 
+FEATURE_DIR = f'../data/processed/wavelet_features/{patient_name}/1_hop/'
+
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser()
     argparser.add_argument('--data_dir', type=str, default='data/interim')
