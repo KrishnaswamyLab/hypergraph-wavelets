40ee535cbfda8ceabeb849173984556ff80f72aa
João Felipe Rocha <joaofelipe.rocha@yale.edu>
2026-03-31 10:36:07 -0400

    Deprecatted torch_scatter. Now we can use native pytorch implementations.

--- src/hypergraphs/hyperedge_aggregator.py
+++ src/hypergraphs/hyperedge_aggregator.py
@@ -1,35 +1,25 @@
 import torch
-from torch_scatter import scatter_mean
+from torch_geometric.utils import scatter
 
 def compute_mean(x, edge_index):
     row, col = edge_index
-    mean_features = scatter_mean(x[row], col, dim=0)
+    mean_features = scatter(x[row], col, dim=0, reduce='mean')
     return mean_features
 
 def compute_var(x, edge_index):
     row, col = edge_index
-    mean_features = scatter_mean(x[row], col, dim=0)
-    mean_sq = scatter_mean((x*x)[row], col, dim=0)
+    mean_features = scatter(x[row], col, dim=0, reduce='mean')
+    mean_sq = scatter((x*x)[row], col, dim=0, reduce='mean')
     var_features = mean_sq - mean_features * mean_features
     return var_features
 
 def compute_cov(x, edge_index):
     row, col = edge_index
-    mean_features = scatter_mean(x[row], col, dim=0)
+    mean_features = scatter(x[row], col, dim=0, reduce='mean')
     xxT = torch.einsum('bi,bj->bij', x, x)
     xxT = xxT[row,...]
     shapes = xxT.size()
     xxT = xxT.reshape(-1, x.size(1) * x.size(1))
-    mean_xxT = scatter_mean(xxT, col, dim=0).reshape(-1, x.size(1), x.size(1))
+    mean_xxT = scatter(xxT, col, dim=0, reduce='mean').reshape(-1, x.size(1), x.size(1))
     cov = mean_xxT - torch.einsum('bi,bj->bij', mean_features, mean_features)
     return cov
-
-"""
-(manual, for testing, slow)
-"""
-def mean_manual(x, edge_index):
-    means = torch.zeros(edge_index[1].max()+1, x.size(1))
-    for i in range(edge_index[1].max()+1):
-        ids = edge_index[0][edge_index[1] == i]
-        means[i,:] = x[ids,:].mean(dim=0)
-    return means
